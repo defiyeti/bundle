ba94d1cd654a1ddd140313263d24ff4583139664
Xiaoyu Yin <yin530@gmail.com>
2015-10-25 09:17:32 -0700

    try to figure something out

--- scripts/services/upc_service.js
+++ scripts/services/upc_service.js
@@ -3,11 +3,12 @@ angular.module('bundle')
     function getItemInfo(upc) {
         var deferred = $q.defer();
         console.log(upc);
-        $http.get('http://www.searchupc.com/handlers/upcsearch.ashx?request_type=3&access_token=C1555391-AB2D-40A7-9575-AF6F819FBF20&upc=' + upc.text)
-        .then(function (res) {
-            console.log(res.data);
-            deferred.resolve(res.data['0']);
-        });
+        // $http.get('http://www.searchupc.com/handlers/upcsearch.ashx?request_type=3&access_token=C1555391-AB2D-40A7-9575-AF6F819FBF20&upc=' + upc.text)
+        // .then(function (res) {
+        //     console.log(res.data);
+        //     deferred.resolve(res.data['0']);
+        // });
+        deferred.resolve(upc);
         return deferred.promise;
     }
 
